824abca2541ddb7c6b1ea21a39bbaac5e9b5de41
Prakash Natarajan <nprakashece@gmail.com>
2025-01-01 19:08:26 -0500

    chore: changed README

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -82,7 +82,7 @@ const config: Config = {
         },
         // { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/prakashn27/jab-docs",
+          href: "https://github.com/prakashn27/scriptflow-docs",
           label: "GitHub",
           position: "right",
         },
